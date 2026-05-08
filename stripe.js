b5b11b523fca830a246d7d5b50c3ae6b7959aba1
JL Shop 06 <greentherapy06@gmail.com>
2026-05-08 13:30:55 +0100

    Update Stripe public key to live key

--- stripe.js
+++ stripe.js
@@ -1,4 +1,4 @@
-const stripe = Stripe("YOUR_PUBLIC_STRIPE_KEY");
+const stripe = Stripe("pk_live_51Q034cF9c1lWA0Hy7EnayZDKTYJUiKnm0vPcOy8mkUGDmq1OjBennoOi7ZKVSmPekJXpvkzB7hZ9Kuwfq4haMEYS004gwlFkJu");
 
 async function subscribe() {
   const response = await fetch("/create-checkout-session", {
