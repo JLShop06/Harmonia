2e2a57f5c8b25d1ef2dc4475ee44b200b8d8b08a
JL Shop 06 <greentherapy06@gmail.com>
2026-05-09 13:54:12 +0100

    Update Supabase URL and key in auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // IMPORTANT : UNE SEULE déclaration
-const supabaseUrl = "TON_URL_SUPABASE";
-const supabaseKey = "TON_ANON_KEY";
+const supabaseUrl = "https://arewzgemzqmokinlylhu.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyZXd6Z2VtenFtb2tpbmx5bGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMzgyNzQsImV4cCI6MjA5MzgxNDI3NH0.vBh00PCILcjrcGynLto-5Ce7zfRvjTXMUDqzG1PWGMw";
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
